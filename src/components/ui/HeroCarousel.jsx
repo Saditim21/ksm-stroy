be5559c90b44ff212e6ba20587c7539f11107b1b
saditim <saditimimam9@gmail.com>
2025-12-01 19:56:36 +0100

    update

--- src/components/ui/HeroCarousel.jsx
+++ src/components/ui/HeroCarousel.jsx
@@ -37,7 +37,7 @@ const HeroCarousel = () => {
     {
       name: "Golden Residence",
       location: "жк ЛЕВСКИ Г | ул. Ген. Климент Бояджиев",
-      apartments: { total: 192, available: 100, reserved: 5, sold: 87 },
+      apartments: { total: 192, available: 127, reserved: 5, sold: 60 },
       garages: { total: 113, available: 78, reserved: 0, sold: 35 }
     }
   ]
